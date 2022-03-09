5e628fe82ef78c64596cc37467febb4d4460b1f0
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2022-03-09 14:10:41 +0300

    Mssql connection for testing

--- DataAccess/Concrete/EntityFramework/UserContext.cs
+++ DataAccess/Concrete/EntityFramework/UserContext.cs
@@ -9,8 +9,9 @@ namespace DataAccess.Concrete.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseNpgsql(@"Host=localhost;Database=BurganUser;Username=postgres;Password=lamal159***");
-            optionsBuilder.UseNpgsql(@"Host=18.192.189.47:5433;Database=yugabyte;Username=admin;Password=rd2OZvpjbrthkDc0ASgiteXnUKCX6Y");
+            //optionsBuilder.UseNpgsql(@"Host=18.192.189.47:5433;Database=yugabyte;Username=admin;Password=rd2OZvpjbrthkDc0ASgiteXnUKCX6Y");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0UBP1P5;Database=SurveyTest;Trusted_Connection=true");
         }
-        public DbSet<User> User { get; set; }
+        public DbSet<User> tblUser { get; set; }
     }
 }
