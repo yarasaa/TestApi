47073745eed47467c0a923e95754b0619aebba1b
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2022-05-07 01:00:26 +0300

    update

--- DataAccess/Concrete/EntityFramework/UserContext.cs
+++ DataAccess/Concrete/EntityFramework/UserContext.cs
@@ -15,8 +15,8 @@ namespace DataAccess.Concrete.EntityFramework
         {
             //optionsBuilder.UseNpgsql(@"Host=localhost;Database=BurganUser;Username=postgres;Password=lamal159***");
             //optionsBuilder.UseNpgsql(@"Host=18.192.189.47:5433;Database=yugabyte;Username=admin;Password=rd2OZvpjbrthkDc0ASgiteXnUKCX6Y");
-            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-0UBP1P5;Database=SurveyTest;Trusted_Connection=true");
-            optionsBuilder.UseSqlServer(@"Server=10.180.20.125;Database=SurveyTest;User Id=DEBUGUSER;Password=aAR=GsG4");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0UBP1P5;Database=SurveyTest;Trusted_Connection=true");
+            //optionsBuilder.UseSqlServer(@"Server=10.180.20.125;Database=SurveyTest;User Id=DEBUGUSER;Password=aAR=GsG4");
             
 
             base.OnConfiguring(optionsBuilder);
--- DataAccess/Concrete/EntityFramework/UserDataContext.cs
+++ DataAccess/Concrete/EntityFramework/UserDataContext.cs
@@ -15,11 +15,11 @@ namespace DataAccess.Concrete.EntityFramework
         {
             //optionsBuilder.UseNpgsql(@"Host=localhost;Database=BurganUser;Username=postgres;Password=lamal159***");
             //optionsBuilder.UseNpgsql(@"Host=18.192.189.47:5433;Database=yugabyte;Username=admin;Password=rd2OZvpjbrthkDc0ASgiteXnUKCX6Y");
-            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-0UBP1P5;Database=SurveyTest;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0UBP1P5;Database=SurveyTest;Trusted_Connection=true");
             //optionsBuilder.UseSqlServer(@"Server=10.180.20.125;Database=SurveyTest;User Id=DEBUGUSER;Password=aAR=GsG4");
             //optionsBuilder.UseSqlServer(@"Server=10.200.0.14;Database=HUMANIST;User Id=DEBUGUSER;Password=aAR=GsG4");
             //optionsBuilder.UseSqlServer(@"Server=10.200.0.14;Database=HUMANIST;Trusted_Connection=true");
-            optionsBuilder.UseSqlServer(@"Server=10.180.20.125;Database=SurveyTest;User Id=DEBUGUSER;Password=aAR=GsG4");
+            //optionsBuilder.UseSqlServer(@"Server=10.180.20.125;Database=SurveyTest;User Id=DEBUGUSER;Password=aAR=GsG4");
 
 
             base.OnConfiguring(optionsBuilder);
